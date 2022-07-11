330e1a5d648aa7807f9ea559f612719f2f45d434
Zayar Lynn <zeiyarlynn@gmail.com>
2022-07-11 21:14:20 +0630

    completed responsive disign for the ranking page

--- components/ThumbNailStats.tsx
+++ components/ThumbNailStats.tsx
@@ -9,6 +9,7 @@ import {
   StatNumber,
   StatUpArrow,
   StatDownArrow,
+  ChakraProps,
 } from '@chakra-ui/react';
 import { ViewIcon, StarIcon } from '@chakra-ui/icons';
 import { ThumbNail } from '../firebaseUtils';
@@ -24,55 +25,82 @@ const ThumbNailStats: React.FC<Props> = ({ id, yt_link, pt, seen, at, by, idx })
   const rating = (pt * 100) / seen;
 
   return (
-    <GridItem key={yt_link} position='relative' borderRadius={5} p={3} borderWidth={1}>
-      <Thumbnail v_link={yt_link} />
-      <StatGroup className='pb-2 pt-5'>
-        <Stat>
-          <StatLabel>
-            Seen <ViewIcon />
-          </StatLabel>
-          <StatNumber>{seen}</StatNumber>
-        </Stat>
-        <Stat>
-          <StatLabel>
-            clicked <StarIcon mb={0.8} />
-          </StatLabel>
-          <StatNumber>{pt}</StatNumber>
-        </Stat>
-        <Stat>
-          <StatLabel>Rating {rating >= 50 ? <StatUpArrow /> : <StatDownArrow />}</StatLabel>
-          <StatNumber>{rating.toFixed(2)}%</StatNumber>
-        </Stat>
-      </StatGroup>
-      <Box mt={4} display='flex' justifyContent='space-between'>
-        <Text fontSize='sm'>
-          submitted by <span className='font-semibold text-primary'>{'by'}</span>
-        </Text>
-        <Flex fontSize={12}>
-          <Text>{d.toLocaleDateString()}</Text>
-          <Text fontSize={14} lineHeight={1.3} fontWeight='medium' mx={1.5}>
-            at
-          </Text>
-          <Text>{d.toLocaleTimeString()}</Text>
-        </Flex>
+    <GridItem
+      key={yt_link}
+      display={{ md: 'flex' }}
+      columnGap={{ md: 4, lg: 5 }}
+      borderRadius={5}
+      borderWidth={1}
+      p={3}
+    >
+      <Box position='relative' flexGrow={1}>
+        <Thumbnail v_link={yt_link} />
+        <Box {...badgeProps as ChakraProps}>
+          <span>{idx + 1}</span>
+        </Box>
       </Box>
-      <Box
-        w={16}
-        h={16}
-        top={0}
-        right={0}
-        fontSize='4xl'
-        display='grid'
-        placeItems='center'
-        position='absolute'
-        shadow='lg'
-        bgColor='black'
-        color='white'
-      >
-        <span>{idx + 1}</span>
+      <Box pl={{ md: 2 }} w={{ md: '15rem' }}>
+        <StatGroup
+          pt={['4vw', 5, 2]}
+          display={['flex', 'flex', 'grid']}
+          gridTemplateColumns='1fr 1fr'
+          columnGap={12}
+          rowGap={{ lg: 3 }}
+        >
+          <Stat>
+            <StatLabel>
+              Seen <ViewIcon />
+            </StatLabel>
+            <StatNumber>{seen}</StatNumber>
+          </Stat>
+          <Stat mb={4}>
+            <StatLabel>
+              clicked <StarIcon mb={0.8} />
+            </StatLabel>
+            <StatNumber>{pt}</StatNumber>
+          </Stat>
+          <Stat>
+            <StatLabel>Rating {rating >= 50 ? <StatUpArrow /> : <StatDownArrow />}</StatLabel>
+            <StatNumber>{rating.toFixed(2)}%</StatNumber>
+          </Stat>
+        </StatGroup>
+        <Box
+          mt={[0, 4, 6, 16]}
+          display='flex'
+          flexWrap='wrap'
+          columnGap={6}
+          rowGap={1}
+          alignItems='center'
+          justifyContent='space-between'
+        >
+          <Text fontSize={[13, 'sm']}>
+            submitted by <span className='font-semibold text-primary'>{by ?? 'a contributer'}</span>
+          </Text>
+          <Flex fontSize={12}>
+            <Text>{d.toLocaleDateString()}</Text>
+            <Text fontSize={14} lineHeight={1.3} fontWeight='medium' mx={1.5}>
+              at
+            </Text>
+            <Text>{d.toLocaleTimeString()}</Text>
+          </Flex>
+        </Box>
       </Box>
     </GridItem>
   );
 };
 
 export default ThumbNailStats;
+
+const badgeProps = {
+  w: [12, 16],
+  h: [12, 16],
+  top: 0,
+  right: 0,
+  fontSize: '4xl',
+  display: 'grid',
+  placeItems: 'center',
+  position: 'absolute',
+  shadow: 'lg',
+  bgColor: 'black',
+  color: 'white',
+};
--- pages/ranking.tsx
+++ pages/ranking.tsx
@@ -3,33 +3,6 @@ import { fetchThumbnails, TFType, ThumbNail } from '../firebaseUtils';
 import { NextPage } from 'next';
 import ThumbNailStats from '../components/ThumbNailStats';
 
-const videos = [
-  {
-    url: 'https://img.youtube.com/vi/sgZjbk9eH6g/hqdefault.jpg',
-    pt: 20,
-    view: 23,
-    yt_link: 'www.youtube.com/watch?v=Otr3Up8wRn0',
-    by: 'mira yoshi',
-    at: 2327439493489,
-  },
-  {
-    url: 'https://img.youtube.com/vi/sgZjbk9eH6g/hqdefault.jpg',
-    pt: 4,
-    view: 34,
-    yt_link: 'www.youtube.com/watch?v=Otr3Up8wRn0',
-    by: 'mira yoshi',
-    at: 58490398,
-  },
-  {
-    url: 'https://img.youtube.com/vi/sgZjbk9eH6g/hqdefault.jpg',
-    pt: 3,
-    view: 34,
-    yt_link: 'www.youtube.com/watch?v=Otr3Up8wRn0',
-    by: 'mira yoshi',
-    at: 95845847,
-  },
-];
-
 const ranking: NextPage<{ rankings: ThumbNail[] }> = ({ rankings }) => {
   return (
     <Box
@@ -39,7 +12,7 @@ const ranking: NextPage<{ rankings: ThumbNail[] }> = ({ rankings }) => {
       h='calc(100vh - 60px)'
       overflowY='scroll'
     >
-      <Box w='90%' py={4} fontSize='lg'>
+      <Box w='min(90%, 27rem)' py={4} fontSize='lg'>
         <span>Top</span>
         <select className='mx-2 w-14 px-1'>
           {[5, 10, 15].map((i) => (
@@ -48,7 +21,15 @@ const ranking: NextPage<{ rankings: ThumbNail[] }> = ({ rankings }) => {
         </select>
         <span>thumbnail</span>
       </Box>
-      <Grid as='main' w='90%' flexGrow={1} fontSize='lg' rowGap={16} mb={20}>
+      <Grid
+        as='main'
+        maxW={['27rem', '27rem', 'none']}
+        w={['90%', '90%', '41rem', '47rem']}
+        flexGrow={1}
+        fontSize='lg'
+        rowGap={16}
+        mb={20}
+      >
         {rankings.map((thumbnail, i) => (
           <ThumbNailStats key={thumbnail.id} {...thumbnail} idx={i} />
         ))}
